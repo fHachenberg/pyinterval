ae18f81775f56215e54c683e2df02f0905689a8d
Fabian Hachenberg <fabian.hachenberg@zeiss.com>
2017-11-21 19:53:16 +0100

    updated example

--- example_instrumentation.sh
+++ example_instrumentation.sh
@@ -1,6 +1,2 @@
-function instrumented_make() {    
-    pyinterval_instrument "make $*"
-    return $?
-}
-# Instrumentierung zum Loggen von unproduktiven Zeiten
-alias make=instrumented_make
+# instrument make
+alias make='instrumented_make "make $*"'
